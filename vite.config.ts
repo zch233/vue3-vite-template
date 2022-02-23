5fb395d31814d2137c0a1be56dd6a4157111d9b2
zhangchao <529743595@qq.com>
2022-02-23 14:51:15 +0800

    chore: set path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { UserConfig, ConfigEnv, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import * as path from 'path';
+
+const resolve = (dir: string) => path.resolve(__dirname, dir)
 
 // https://vitejs.dev/config/
 export default ({mode}: ConfigEnv):UserConfig => {
@@ -8,5 +11,10 @@ export default ({mode}: ConfigEnv):UserConfig => {
   console.log(env);
   return {
     plugins: [vue(), vueJsx()],
+    resolve: {
+      alias: {
+        "@src": resolve('./src'),
+      }
+    }
   }
 }
