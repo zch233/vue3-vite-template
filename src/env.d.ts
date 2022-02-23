1ff2eefa3ea48accf3f324713c9ef45aa483aa44
zhangchao <529743595@qq.com>
2022-02-23 14:42:35 +0800

    chore: load env files

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,3 +6,14 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+
+interface ImportMetaEnv {
+  readonly VITE_APP_TITLE: string
+  readonly VITE_APP_TAG: string
+  readonly VITE_APP_API_URL: string
+  readonly VITE_APP_VUEX_PERSISTED_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
+import { UserConfig, ConfigEnv, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx()],
-})
+export default ({mode}: ConfigEnv):UserConfig => {
+  const env = loadEnv(mode, process.cwd())
+  console.log(env);
+  return {
+    plugins: [vue(), vueJsx()],
+  }
+}
