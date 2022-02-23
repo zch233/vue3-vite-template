7db40f83772df0f1cb2bd5bd940df1600f208295
zhangchao <529743595@qq.com>
2022-02-23 16:01:17 +0800

    chore: inject html data

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,28 @@ import { UserConfig, ConfigEnv, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import * as path from 'path';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 const resolve = (dir: string) => path.resolve(__dirname, dir)
 
 // https://vitejs.dev/config/
-export default ({mode}: ConfigEnv):UserConfig => {
-  const env = loadEnv(mode, process.cwd())
-  console.log(env);
+export default ({mode, command}: ConfigEnv):UserConfig => {
+  const {VITE_APP_TITLE} = loadEnv(mode, process.cwd())
+  const isBuild = command === 'build';
   return {
-    plugins: [vue(), vueJsx()],
+    plugins: [
+      vue(),
+      vueJsx(),
+      createHtmlPlugin({
+        minify: isBuild,
+        inject: {
+          // Inject data into ejs template
+          data: {
+            title: VITE_APP_TITLE,
+          },
+        },
+      })
+    ],
     resolve: {
       alias: {
         "@src": resolve('./src'),
