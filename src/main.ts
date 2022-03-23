309b84a040de32af461adad2f9599c409ffc39fe
zhangchao <529743595@qq.com>
2022-03-23 17:11:45 +0800

    feat: add vue router & demo

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App'
 import 'virtual:svg-icons-register' // Here the svg sprite map has been generated
+import { router } from '@src/router';
+import '@src/router/routerGuard';
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
---
+++ src/router/routerGuard.ts
@@ -0,0 +1,13 @@
+import { router } from '@src/router/index';
+import {isNavigationFailure} from 'vue-router'
+
+router.beforeEach((to) => {
+  if (to.path === '/homeAgain') {
+    return {path: '/'}
+  }
+})
+router.afterEach((to, from, failure) => {
+  if (isNavigationFailure(failure)) {
+    console.error('路由错误啦',failure)
+  }
+})
