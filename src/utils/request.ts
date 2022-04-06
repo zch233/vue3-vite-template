a593409ed28d6a42f40abeb9a01d4ad9507f73b2
yejun <2512636001@qq.com>
2022-04-06 16:07:13 +0800

    feat: add request type

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,22 +1,32 @@
-import axios from 'axios';
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse, AxiosPromise } from 'axios';
 import { router } from '@src/router';
 import { useUser } from '@src/store/modules/user';
 
-const user = useUser()
-export const request = axios.create({
-    baseURL: process.env.VUE_APP_API_URL,
+interface CustomAxiosRequestConfig extends AxiosRequestConfig {
+    banRepeatCancel?: boolean;
+}
+
+interface CustomAxiosInstance extends AxiosInstance {
+    (config: CustomAxiosRequestConfig): AxiosPromise;
+    (url: string, config?: CustomAxiosRequestConfig): AxiosPromise;
+}
+
+const user = useUser();
+export const request: CustomAxiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_APP_API_URL,
     timeout: 120 * 1000,
 });
 
-export const delayRequest = axiosRequestConfig => ({
-    start: () => request({ ...axiosRequestConfig, isDelayRequest: true }),
+export const delayRequest = (axiosRequestConfig: CustomAxiosRequestConfig) => ({
+    start: () => request({ ...axiosRequestConfig, ...{ isDelayRequest: true } }),
     cancel: () => removePending(axiosRequestConfig),
 });
 
 export const pending = new Map();
-export const generateURL = config => [config.method, config.url.replace(process.env.VUE_APP_API_URL, '')].filter(Boolean).join('&');
+export const generateURL = (config: CustomAxiosRequestConfig) =>
+    [config.method, config.url?.replace(import.meta.env.VITE_APP_API_URL, '')].filter(Boolean).join('&');
 
-const addPending = config => {
+const addPending = (config: CustomAxiosRequestConfig) => {
     const url = generateURL(config);
     config.cancelToken =
         config.cancelToken ||
@@ -31,7 +41,7 @@ const addPending = config => {
  * 移除请求
  * @param {Object} config
  */
-const removePending = config => {
+const removePending = (config: CustomAxiosRequestConfig) => {
     const url = generateURL(config);
     if (pending.has(url)) {
         // 如果在 pending 中存在当前请求标识，且没有禁用，需要取消当前请求，并且移除
@@ -48,10 +58,10 @@ export const clearPending = () => {
 };
 
 request.interceptors.request.use(
-    config => {
+    (config: CustomAxiosRequestConfig) => {
         const token = user.name;
         if (token) {
-            config.headers.common['Authorization'] = token;
+            (config.headers?.common as any)['Authorization'] = token;
         }
         if (!config.banRepeatCancel) removePending(config); // 在请求开始前，对之前的请求做检查取消操作
         addPending(config); // 将当前请求添加到 pending 中
@@ -66,7 +76,7 @@ request.interceptors.request.use(
 
 // Add a response interceptor
 request.interceptors.response.use(
-    response => {
+    (response: AxiosResponse) => {
         // Do something with response data
         removePending(response.config); // 在请求结束后，移除本次请求
         const data = response.data;
