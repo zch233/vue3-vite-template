907e1b82d35d0b1f82cbee7127a84c5891cfdd8d
zhangchao <529743595@qq.com>
2022-05-12 16:12:12 +0800

    refactor: split plugins

--- config/vite/plugins/index.ts
+++ config/vite/plugins/index.ts
@@ -1,5 +1,5 @@
-import vue from '@vitejs/plugin-vue';
-import vueJsx from '@vitejs/plugin-vue-jsx';
+import { configPluginVue } from './pluginVue';
+import { configPluginVueJsx } from './pluginVueJsx';
 import { ConfigEnv } from 'vite';
 import { configPluginHtml } from './vitePluginHtml';
 import { configSvgIconsPlugin } from './vitePluginSvgIcons';
@@ -9,27 +9,27 @@ import { configPluginVisualizer } from './rollupPluginVisualizer';
 import { configPluginCompression } from './vitePluginCompression';
 import { configPluginWindicss } from './vitePluginWindicss';
 import { configPluginCertificate } from './vitePluginMkcert';
-import vueSetupExtend from 'vite-plugin-vue-setup-extend';
 import { configPluginUnpluginComponents } from './unpluginVueComponents';
 import { configPluginUnpluginImport } from './unpluginAutoImport';
+import { configPluginVueSetupExtend } from './vitePluginVueSetupExtend';
 
 export const createVitePlugins = ({ mode, command }: ConfigEnv, viteEnv: ViteEnv) => {
     const isBuild = command === 'build';
-    const { VITE_WINDICSS, VITE_SEE_VISUALIZER, VITE_LEGACY, VITE_USE_IMAGEMIN, VITE_LISTEN_HTTPS, VITE_UNPLUGINS_COMPONENTS, VITE_UNPLUGINS_IMPORTS } = viteEnv;
+    const { VITE_WINDICSS, VITE_SEE_VISUALIZER, VITE_LEGACY, VITE_USE_IMAGEMIN, VITE_LISTEN_HTTPS, VITE_UNPLUGINS_COMPONENTS, VITE_UNPLUGINS_IMPORTS } =
+        viteEnv;
     // https://github.com/vitejs/awesome-vite#plugins
     // vite-plugin-pages // 自动根据目录生成路由
-    // unplugin-auto-import // 依赖按需自动导入
     const plugins = [
-        vue(),
-        vueJsx(),
-        vueSetupExtend(), // 支持在 setup 上使用组件 name
+        configPluginVue(),
+        configPluginVueJsx(),
+        configPluginVueSetupExtend(), // 支持在 setup 上使用组件 name
         configPluginHtml(isBuild, viteEnv),
         configSvgIconsPlugin(isBuild),
     ];
 
-    if (VITE_UNPLUGINS_COMPONENTS) plugins.push(configPluginUnpluginComponents(viteEnv))
+    if (VITE_UNPLUGINS_COMPONENTS) plugins.push(configPluginUnpluginComponents(viteEnv));
 
-    if (VITE_UNPLUGINS_IMPORTS) plugins.push(configPluginUnpluginImport())
+    if (VITE_UNPLUGINS_IMPORTS) plugins.push(configPluginUnpluginImport());
 
     if (VITE_LISTEN_HTTPS) plugins.push(configPluginCertificate());
 
