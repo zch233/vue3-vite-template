e489e40b6e782df5e5ae3fced17fb62ca678d5bf
zhangchao <529743595@qq.com>
2022-06-02 14:29:31 +0800

    chore: update template

--- README.md
+++ README.md
@@ -7,6 +7,15 @@
 ## 如何使用该模版？
 
 1. 首先切换到相应的模版分支
-2. 运行 `./template.sh git@xxxxxxx.git`
+2. 运行
+
+    ```
+    // 1. 当前无 git 仓库，在本地目录创建
+    ./template.sh [你的文件夹名称] // 文件夹名称可选，默认为当前日期时间
+
+    // 2. 当前已经有 git 仓库存在了
+    ./template.sh git@xxxxxxx.git // 新仓库的 codeup url。注意：请使用 ssh 协议！
+    ```
+
 3. 静静等待
 4. 完
--- template.sh
+++ template.sh
@@ -2,26 +2,43 @@
 
 # 确保脚本抛出遇到的错误
 set -e
-branchName="feature/`date +%Y-%m-%d`"
-tempRemoteName="template/`date +%Y-%m-%d-%H-%m-%s`"
-currentBranch=`git symbolic-ref --short -q HEAD`
 
 gitUrl=$1
-urlArray=(${gitUrl//// })
-urlArrayLastIndex=$((${#urlArray[@]}-1))
-lastString=${urlArray[urlArrayLastIndex]}
-lastStringArray=(${lastString//./ })
-dirName=${lastStringArray[0]}
+gitUrl=${gitUrl:-`date +%Y-%m-%d-%H-%m-%s`}
 
-git pull &&
-git remote add $tempRemoteName $gitUrl &&
-git push $tempRemoteName $currentBranch:$branchName &&
-git remote remove $tempRemoteName &&
-cd .. &&
-git clone $1 &&
-cd $dirName &&
-git checkout -b $branchName origin/$branchName
-start . &&
-code . &&
-yarn &&
-echo "恭喜你，创建成功"
+if [ "${gitUrl/#git@}" = "$gitUrl" ]
+then
+    echo "本地路径创建模版"
+    mkdir ../$gitUrl &&
+    cp -r `ls -A | grep .| grep -v ".git"| grep -v "node_modules"| grep -v ".idea"` ../$gitUrl/ &&
+    cd ../$gitUrl &&
+    git init &&
+    start . &&
+    code . &&
+    yarn &&
+    echo "恭喜你，创建成功"
+else
+    echo "git仓库创建模版"
+    branchName="feature/`date +%Y-%m-%d`"
+    tempRemoteName="template/`date +%Y-%m-%d-%H-%m-%s`"
+    currentBranch=`git symbolic-ref --short -q HEAD`
+
+    urlArray=(${gitUrl//// })
+    urlArrayLastIndex=$((${#urlArray[@]}-1))
+    lastString=${urlArray[urlArrayLastIndex]}
+    lastStringArray=(${lastString//./ })
+    dirName=${lastStringArray[0]}
+
+    git pull &&
+    git remote add $tempRemoteName $gitUrl &&
+    git push $tempRemoteName $currentBranch:$branchName &&
+    git remote remove $tempRemoteName &&
+    cd .. &&
+    git clone $1 &&
+    cd $dirName &&
+    git checkout -b $branchName origin/$branchName
+    start . &&
+    code . &&
+    yarn &&
+    echo "恭喜你，创建成功"
+fi
